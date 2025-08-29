48749af8a88bab27b8adbb381c327d470a776a8c
iyyappanmanivelan <iyyappanmani03@gmail.com>
2025-08-29 12:23:40 +0530

    document.js changes

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,13 +1,50 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="/plugins/bootstrap/css/bootstrap.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          onLoad="this.media='all'"
+        />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=YourFont&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"
+          onLoad="this.media='all'"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"
+          onLoad="this.media='all'"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        strategy="lazyOnload"
+      />
+      <script src="/plugins/slick-carousel/slick/slick.min.js"></script>
     </Html>
   );
 }
